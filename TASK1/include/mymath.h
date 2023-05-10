5999fc2466525292fecca4e5e036a114c1e3a7ba
hamna noor <hamna@rakurai.io>
2023-05-10 16:52:38 +0500

    Revert "added task 1 files"
    This reverts commit 6941d98a3f4bf681e5e3c42751c8e45510921387.

--- TASK1/include/mymath.h
+++
@@ -1,9 +0,0 @@
-
-// mymath.h
-#ifndef MYMATH_H
-#define MYMATH_H
-namespace mymath{
-int add(int a, int b);
-int subtract(int a, int b);
-}
-#endif
--- TASK1/src/mymath.cpp
+++
@@ -1,5 +0,0 @@
-
-// mymath.cpp
-#include "mymath.h"
-int mymath::add(int a, int b) {    return a + b;}
-int mymath::subtract(int a, int b) {    return a - b;}
